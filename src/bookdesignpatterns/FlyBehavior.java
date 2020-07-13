57a01a3b456d28d9a1c3bafb0b00ba2c6fcb3fc5
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-07-13 21:53:52 +0200

    Bock - Design Patterns - start with ducks

---
+++ src/bookdesignpatterns/FlyBehavior.java
@@ -0,0 +1,5 @@
+package bookdesignpatterns;
+
+public interface FlyBehavior {
+	public void fly();
+}
---
+++ src/bookdesignpatterns/QuackBehavior.java
@@ -0,0 +1,5 @@
+package bookdesignpatterns;
+
+public interface QuackBehavior {
+	public void quack();
+}
