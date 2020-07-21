6e0001813670313f0be4bd159a3edcfd768de5e9
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-07-21 23:00:37 +0200

    templates / compareTo

--- src/bookdesignpatternsadapter/DuckTestDrive.java
+++ src/bookdesignpatternsadapter/DuckTestDrive.java
@@ -15,20 +15,26 @@ public class DuckTestDrive {
 		// and 2) the duck in a DuckAdapter, which makes it look like a Turkey...
 		Turkey duckAdapter = new DuckAdapter (duck);
 		
+		
+		
 		System.out.println("The Turkey says...");
 		turkey.gobble();
 		turkey.fly();
 		
+		
 		System.out.println("\nThe Duck says ...");
 		testDuck(duck);
 		
+		
 		System.out.println("\nThe TurkeyAdapter says...");
 		testDuck(turkeyAdapter);
+		
 		
 		System.out.println("\nThe DuckAdapter says ...");
 		testTurkey(duckAdapter);
 	}
 
+	
 	
 	private static void testTurkey(Turkey turkey) {
 		turkey.gobble();
