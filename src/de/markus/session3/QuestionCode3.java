2d50ad24e4fc8a4b5903f0eb335943c1547dad76
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-08-31 21:27:58 +0200

    small changes only

--- src/de/markus/session3/QuestionCode3.java
+++ src/de/markus/session3/QuestionCode3.java
@@ -33,67 +33,6 @@ public class QuestionCode3 {
 		// . ich rufe auf einer Instanz A.D eine Methode auf.
 		
 		
-		/*
-		
-		
-		
-		
-		// ===============================================================================================
-		 
-		==>> Können wir so etwas einmal bauen:
-
-		A.B() {
-			public void b(C c, D d) {
-				System.out.println("Zeige etwas...");
-			}
-		}
-		
-		// ===============================================================================================
-		// Beispiel-Code:
-		IabHelper.QueryInventoryFinishedListener() {
-			public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
-				
-			}
-		
-		// ===============================================================================================
-		
-		
-		
-		
-		
-		
-		
-		
-		Frage: 
-		Warum schreibe ich überhaupt Klasse(n)? Ich könnte ein Programm mit Methoden schreiben. 
-		
-			--> A Class is like an object constructor, or a "blueprint" for creating objects.
-		
-		
-		Und wenn ich das weiß, dann weiß ich wozu anonyme Klassen gut sind !
-		
-			--> A anonymous class is used only once at exactly that place.
-		
-		
-		Create an Object
-		In Java, an object is created from a class. We have already created the class named MyClass, so now we can use this to create objects.
-		To create an object of MyClass, specify the class name, followed by the object name, and use the keyword new:
-		
-		
-		// =======================================================================//
-		//Themen:
-		  - ( REST Controller ) / REST API
-		  - Autowire / Rest-Zugriff / Repository-Zugriff / WebClient / Web-Request --> Service-Klasse
-		  - LiveData
-		  - Observer Pattern
-		  - Rest
-		  - Asyncron / delay
-		  - Thread handling
-		
-		
-		
-		*/
-		
 		
 		// innere Klasse
 		class MyClass {
@@ -102,11 +41,8 @@ public class QuestionCode3 {
 		
 		// geht dies nur NACH der inneren Klasse "MyClass" ???
 		MyClass myObj = new MyClass();
-		System.out.println(myObj.x);
+		System.out.println("\n" + myObj.x + "<== just another example with myObj AND an inner Class ... (has nothing to do with the lines before!");
 			  
-			  
-		
-		
 		
 	}
 
